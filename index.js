4405f69bb1dc9408db41793be12e7b8b1bd4a097
Farhaan <farhaanthestar@gmail.com>
2021-11-02 12:37:10 +0530

    Update index.js

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const express = require("express");
 const app = express();
 const path = require("path");
 const fs = require("fs");
+const port = process.env.PORT || 5000;
 
 // svg and varient
 app.get("/:coin", (req, res) => {
@@ -80,6 +81,6 @@ app.get("/:size/:varient/:coin", (req, res) => {
   });
 });
 
-app.listen(3000, function () {
-  console.log("Listening on http://localhost:3000/");
+app.listen(port, function () {
+  console.log("Listening on Port: " + port);
 });
